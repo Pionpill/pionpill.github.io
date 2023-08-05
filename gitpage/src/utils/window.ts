080ff94ca822a8857dfbf3ab73f70c7c88897e9c
pionpill <673486387@qq.com>
2023-08-05 23:41:42 +0800

    :construction: change scroll behavior

--- gitpage/src/utils/window.ts
+++ gitpage/src/utils/window.ts
@@ -6,6 +6,6 @@ export const copyCurrentUrl = () => {
 export const scrollToAnchor = (anchor: string) => {
   const element: HTMLElement | null = document.getElementById(anchor);
   if (element) {
-    element.scrollIntoView({ behavior: "instant", block: "start" });
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
   }
 };
