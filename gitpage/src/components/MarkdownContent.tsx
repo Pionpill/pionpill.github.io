5bee30a882a5e47e4e1243bffff5657e83ea2ae5
pionpill <673486387@qq.com>
2023-08-05 23:40:12 +0800

    :construction: copy code, display code type

--- gitpage/src/components/MarkdownContent.tsx
+++ gitpage/src/components/MarkdownContent.tsx
@@ -1,7 +1,10 @@
 import styled from "@emotion/styled";
-import { Link, Typography } from "@mui/material";
-import React, { PropsWithChildren } from "react";
+import { Alert, IconButton, Link, Snackbar, Typography } from "@mui/material";
+import React, { MouseEventHandler, PropsWithChildren } from "react";
+import { Trans } from "react-i18next";
+import { BiCopy } from "react-icons/bi";
 import ReactMarkdown from "react-markdown";
+import { CodeProps } from "react-markdown/lib/ast-to-react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import rehypeKatex from "rehype-katex";
@@ -10,6 +13,7 @@ import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 import useThemeChoice from "../hooks/useThemeChoice";
 import { getTocAnchor } from "../utils/toc";
+import FlexBox from "./FlexBox";
 
 const MarkdownContentWrapper = styled('div')`
   .katex-html {
@@ -30,7 +34,88 @@ const MarkdownContentWrapper = styled('div')`
   .hint {
     opacity: 0.75;
   }
+  .markdown-code {
+    position: relative;
+    &:hover {
+      .markdown-code-copy {
+        opacity: 0.5;
+        transition: opacity 0.3s ease;
+      }
+      .markdown-code-language {
+        opacity: 0;
+        transition: opacity 0.3s ease;
+      }
+    }
+  }
+  .markdown-code-copy {
+    transition: opacity 0.3s ease;
+    transition: background-color 0.3s ease;
+    position: absolute;
+    top: 16px;
+    right: 16px;
+    opacity: 0;
+    color: white;
+    &:hover {
+      background-color: #555;
+    }
+  }
+
+  .markdown-code-language {
+    transition: opacity 0.3s ease;
+    transition: background-color 0.3s ease;
+    position: absolute;
+    top: 16px;
+    right: 16px;
+    opacity: 0.5;
+    color: white;
+    &:hover {
+      background-color: #555;
+    }
+  }
 `
+
+const MarkdownCode: React.FC<CodeProps & { setAlert: Function }> = ({ children, node, inline, className, setAlert, ...props }) => {
+  const match = /language-(\w+)/.exec(className || "");
+  
+  const handleClick: MouseEventHandler = () => {
+    navigator.clipboard.writeText(String(children));
+    setAlert(true);
+  };
+
+  return !inline && match ? (
+    <FlexBox width="100%" className="markdown-code">
+      <IconButton className="markdown-code-copy" onClick={handleClick}>
+        <BiCopy />
+      </IconButton>
+      <Typography className="markdown-code-language">
+        {match[1]}
+      </Typography>
+      <SyntaxHighlighter
+        {...props}
+        style={oneDark}
+        customStyle={{ width: "100%" }}
+        language={match[1]}
+        PreTag="div"
+      >
+        {String(children).replace(/\n$/, "")}
+      </SyntaxHighlighter>
+    </FlexBox>
+  ) : (
+    <Typography
+      style={{
+        fontSize: "14px",
+        fontFamily: "consolas",
+        padding: "2px 4px",
+        borderRadius: 4,
+        background: useThemeChoice("#f1f1f1", "#2f2f2f"),
+        color: useThemeChoice("#1f1f1f", "#aac8e4"),
+        display: "inline",
+      }}
+    >
+      {children}
+    </Typography>
+  );
+}
 
 const MarkdownBlockquote: React.FC<PropsWithChildren> = ({ children }) => {
   return (
@@ -50,23 +135,35 @@ const MarkdownBlockquote: React.FC<PropsWithChildren> = ({ children }) => {
 };
 
 const MarkdownContent: React.FC<{ children: string }> = ({ children }) => {
+  const [successCopyAlertOpen, setSuccessCopyAlertOpen] = React.useState<boolean>(false);
+
   return (
     <MarkdownContentWrapper>
+      <Snackbar
+        autoHideDuration={3000}
+        onClose={() => setSuccessCopyAlertOpen(false)}
+        open={successCopyAlertOpen}
+      >
+        <Alert severity="success">
+          <Trans i18nKey="common.success" />
+          :&nbsp;
+          <Trans i18nKey="blog.copyCodeSuccessMessage" />
+        </Alert>
+      </Snackbar>
       <ReactMarkdown
         remarkPlugins={[remarkGfm, remarkMath]}
         rehypePlugins={[rehypeKatex, rehypeRaw]}
-
         components={{
-          h2: (props) => {
-            const anchor = getTocAnchor(props.children);
-            return <h2 id={anchor}>{props.children}</h2>;
+          h2: ({children}) => {
+            const anchor = getTocAnchor(children);
+            return <h2 id={anchor}>{children}</h2>;
           },
-          h3: (props) => {
-            const anchor = getTocAnchor(props.children);
-            return <h3 id={anchor}>{props.children}</h3>;
+          h3: ({children}) => {
+            const anchor = getTocAnchor(children);
+            return <h3 id={anchor}>{children}</h3>;
           },
-          img: (props) => (
-            <img src={props.src} alt={props.alt} style={{ maxWidth: "100%" }} />
+          img: ({src, alt}) => (
+            <img src={src} alt={alt} style={{ maxWidth: "100%" }} />
           ),
           a: ({ href, target, children }) => (
             <Link
@@ -79,33 +176,8 @@ const MarkdownContent: React.FC<{ children: string }> = ({ children }) => {
               {children}
             </Link>
           ),
-          blockquote: (props) => <MarkdownBlockquote children={props.children} />,
-          code: ({ node, inline, className, children, ...props }) => {
-            const match = /language-(\w+)/.exec(className || "");
-            return !inline && match ? (
-              <SyntaxHighlighter
-                {...props}
-                children={String(children).replace(/\n$/, "")}
-                style={oneDark}
-                language={match[1]}
-                PreTag="div"
-              />
-            ) : (
-              <Typography
-                style={{
-                  fontSize: "14px",
-                  fontFamily: "consolas",
-                  padding: "2px 4px",
-                  borderRadius: 4,
-                  background: useThemeChoice("#f1f1f1", "#2f2f2f"),
-                  color: useThemeChoice("#1f1f1f", "#aac8e4"),
-                  display: "inline",
-                }}
-              >
-                {children}
-              </Typography>
-            );
-          },
+          blockquote: ({ children }) => <MarkdownBlockquote children={children} />,
+          code: ({ node, inline, className, children, ...props }) => <MarkdownCode node={node} inline={inline} className={className} children={children} {...props} setAlert={setSuccessCopyAlertOpen} />,
         }}
         children={children}
       />
