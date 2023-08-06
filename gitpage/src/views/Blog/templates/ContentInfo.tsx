0e1f48c109af0ba01f8b4c8339ca2a54f3f93c4d
pionpill <673486387@qq.com>
2023-08-06 19:50:13 +0800

    :construction: blog: add pre blog button

--- gitpage/src/views/Blog/templates/ContentInfo.tsx
+++ gitpage/src/views/Blog/templates/ContentInfo.tsx
@@ -1,15 +1,17 @@
-import { Skeleton, Typography } from "@mui/material";
+import { Button, Skeleton, Typography } from "@mui/material";
 import { PropsWithChildren, ReactNode } from "react";
 import { Trans } from "react-i18next";
 import { AiFillTags } from "react-icons/ai";
 import { BsCalendarDateFill } from "react-icons/bs";
+import { FaClipboardList } from "react-icons/fa";
 import { MdCategory, MdTextSnippet, MdTimelapse, MdUpdate } from "react-icons/md";
-import { useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import FlexBox from "../../../components/FlexBox";
 
 // 后续持续增加 meta
 export type ContentInfoType = {
   difficulty?: string;
+  pre?: string;
 };
 
 const ContentInfo: React.FC<{
@@ -18,8 +20,11 @@ const ContentInfo: React.FC<{
   createData: string;
   updateDate: string;
 }> = ({ meta, wordCount, createData, updateDate }) => {
-  const { difficulty } = meta;
+  const { difficulty, pre } = meta;
+  const navigate = useNavigate();
   const locationPath = useLocation().pathname;
+  const preList = pre?.split(" ");
+  console.log(preList);
 
   const difficultyTextSelector = () => {
     return difficulty === "easy"
@@ -56,26 +61,46 @@ const ContentInfo: React.FC<{
   };
 
   return (
-    <FlexBox alignItems="center" flexWrap="wrap" columnGap={1.5}>
-      {difficulty ? (
-        <ContentTag icon={<MdCategory />} title="难度">
-          {difficultyTextSelector()}
+    <FlexBox flexDirection="column">
+      <FlexBox alignItems="center" flexWrap="wrap" columnGap={1.5}>
+        {difficulty ? (
+          <ContentTag icon={<MdCategory />} title="难度">
+            {difficultyTextSelector()}
+          </ContentTag>
+        ) : <Skeleton width={50} />}
+        <ContentTag icon={<AiFillTags />} title="类型"><Trans i18nKey={`blog.${locationPath.split("/")[2]}`} />
         </ContentTag>
-      ) : <Skeleton width={50} />}
-      <ContentTag icon={<AiFillTags />} title="类型"><Trans i18nKey={`blog.${locationPath.split("/")[2]}`} />
-      </ContentTag>
-      <ContentTag icon={<MdTextSnippet />} title="字数">
-        约 {wordCount} 字
-      </ContentTag>
-      <ContentTag icon={<MdTimelapse />} title="阅读时间">
-        约 {calculateReadTime()} 分钟
-      </ContentTag>
-      <ContentTag icon={<BsCalendarDateFill />} title="创建事件">
-        {createData}
-      </ContentTag>
-      <ContentTag icon={<MdUpdate />} title="更新时间">
-        {updateDate}
-      </ContentTag>
+        <ContentTag icon={<MdTextSnippet />} title="字数">
+          约 {wordCount} 字
+        </ContentTag>
+        <ContentTag icon={<MdTimelapse />} title="阅读时间">
+          约 {calculateReadTime()} 分钟
+        </ContentTag>
+        <ContentTag icon={<BsCalendarDateFill />} title="创建事件">
+          {createData}
+        </ContentTag>
+        <ContentTag icon={<MdUpdate />} title="更新时间">
+          {updateDate}
+        </ContentTag>
+      </FlexBox>
+      {
+        preList &&
+        <FlexBox alignItems="center" gap={1} sx={{ opacity: 0.75 }}>
+          <FaClipboardList />
+          <Typography variant="subtitle2">前置博客: </Typography>
+          {
+            preList.map((item) => {
+              const preType = item[0] === "-" ? "optional" : item[0] === "+" ? "necessary" : "common";
+              const blogName = item.split("/").pop()?.split("_").pop();
+              const path = preType === "common" ? `/blog${item}` : `/blog${item.substring(1)}`
+              return (
+                <Button onClick={() => navigate(path)} color={preType === "necessary" ? "error" : preType === "optional" ? "success" : "primary"}>
+                  {blogName}
+                </Button>)
+            })
+          }
+        </FlexBox>
+      }
     </FlexBox>
   );
 };
